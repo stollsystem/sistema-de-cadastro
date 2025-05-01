9ce22914462652832631ae094785dd11d4583b76
stollsystem <renato_stoll@hotmail.com>
2025-04-30 22:34:38 -0300

    Commit com alteração no app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("A aplicacao esta conectando no banco de dados")
+
+console.log("Feita mais uma alteração")
+console.log("Feita novamente mais uma alteração")
+
